884631fcdc28ae56dba802ec01fef456678249f1
João Vitor da Silva <jaumm.silva.04@gmail.com>
2023-02-06 18:27:26 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContatosMVC.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult DeletarConfirmacao()
+        {
+            return View();
+        }
+
+
+    }
+}
